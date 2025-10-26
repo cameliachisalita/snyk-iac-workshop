44e13901af8a37045debc016fb5a34e32fddaabd
cameliachisalita <31044146+cameliachisalita@users.noreply.github.com>
2025-10-26 22:15:31 +0200

    Update big_data.tf

--- terraform/big_data.tf
+++ terraform/big_data.tf
@@ -6,7 +6,7 @@ resource google_sql_database_instance "master_instance" {
   settings {
     tier = "db-f1-micro"
     ip_configuration {
-      ipv4_enabled = false
+      ipv4_enabled = true
       require_ssl = true
       authorized_networks {
         name  = "WWW"
